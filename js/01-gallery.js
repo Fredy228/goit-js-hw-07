047673418e7b44e087bb5b0d60336a55691d8a4e
Alexei Smahrovych <smagrovich58@gmail.com>
2022-10-11 22:00:55 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const markup = galleryItems
+    .map(item => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </div>`
+    })
+    .join("");
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+
+galleryContainer.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+    
+    const instance = basicLightbox.create(`
+    <img
+    class="gallery__image"
+    src="${event.target.dataset.source}"
+    alt="${event.target.alt}"
+  />
+    `);
+    instance.show();
+    window.addEventListener("keydown", (event) => {
+        if (event.code === 'Escape') {
+            instance.close()
+            console.log(event.code);
+        } 
+    }, {once: true});
+});
+
