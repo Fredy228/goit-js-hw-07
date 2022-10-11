ac731bb2c53f7a76a8fa45f054bf1d7f70475361
Alexei Smahrovych <smagrovich58@gmail.com>
2022-10-11 22:35:25 +0300

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const markup = galleryItems
+    .map(item => {
+        return `<a class="gallery__item" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+          />
+        </a>`
+    })
+    .join("");
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+ });
+
+
